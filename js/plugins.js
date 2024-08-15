991d819e20e705a869883733f96210887dfdaa2c
Kamal Malek <kamal.malek@hotmail.com>
2024-08-15 17:55:42 +0300

    trial to fix loading screen 2

--- js/plugins.js
+++ js/plugins.js
@@ -261,3 +261,52 @@ https://github.com/imakewebthings/waypoints/blog/master/licenses.txt
 
 
 
+const helloWorldExamples = [
+  'std::cout << "Hello, World!" << std::endl;',  // C++
+  'print("Hello, World!")',                     // Python
+  'console.log("Hello, World!");',              // JavaScript
+  'System.out.println("Hello, World!");',       // Java
+  'echo "Hello, World!";',                      // PHP
+  'printf("Hello, World!\\n");',                // C
+  'puts "Hello, World!"',                       // Ruby
+  'Console.WriteLine("Hello, World!");',        // C#
+  'fmt.Println("Hello, World!")',               // Go
+  'echo "Hello, World!"',                       // Bash
+];
+
+
+
+document.addEventListener("DOMContentLoaded", function() {
+  var text = helloWorldExamples[Math.floor(Math.random() * helloWorldExamples.length)];
+  var text2 = "> Hello, World!";
+  var speed = 125; // Adjust the typing speed (in milliseconds)
+  var delay = 1250;
+  var appearIn = 500;
+  var delay2 = delay + text.length * speed * 1.1 + appearIn;
+
+  // Function to simulate typing effect
+  function typeEffect() {
+      var i = 0;
+      setTimeout(function () {
+          var typingInterval = setInterval(function () {
+              if (i < text.length) {
+                  document.getElementById("typing-text").innerHTML += text.charAt(i);
+                  i++;
+              } else {
+                  clearInterval(typingInterval);
+              }
+          }, speed);
+      }, delay); // Delay of 1 second before starting the typing effect
+      document.getElementById("typing-text").innerHTML = "";
+      setTimeout(function () {
+          if (text === document.getElementById("typing-text").innerHTML) {
+              document.getElementById("typing-text").innerHTML = text2;
+          }else{
+              delay2 +=250;
+          }
+      }, delay2)
+  }
+
+  // Call the function to start the typing effect
+  typeEffect();
+});
